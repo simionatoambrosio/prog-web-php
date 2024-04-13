dd30d6388ce46dcd24869b88e62e391381064e27
simionatoambrosio <gabriel252004@Outlook.com>
2024-04-13 18:24:04 -0300

    Adicionando o arquivo .sql do banco de dados exportado

--- banco.sql
+++ banco.sql
@@ -1,47 +1,198 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/04/2024 às 23:23
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`categoria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`categoria` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `mydb`.`produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`produto` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(150) NOT NULL,
-  `valor` DECIMAL(8,2) NOT NULL,
-  `categoria_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_produto_categoria_idx` (`categoria_id` ASC),
-  CONSTRAINT `fk_produto_categoria`
-    FOREIGN KEY (`categoria_id`)
-    REFERENCES `mydb`.`categoria` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `mydb`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(256) NOT NULL,
+  `telefone` varchar(11) NOT NULL,
+  `email` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nome`, `telefone`, `email`) VALUES
+(1, 'Teste', '18997113590', 'gabriel@email.com'),
+(2, 'Gabriel', '18997113590', 'gabriel@email.com'),
+(3, 'Arthur', '99999999999', 'arthur@email.com'),
+(4, 'Guilherme', '99999999999', 'guilherme@email.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `despesa`
+--
+
+CREATE TABLE `despesa` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(256) NOT NULL,
+  `valor` decimal(10,0) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `despesa`
+--
+
+INSERT INTO `despesa` (`id`, `descricao`, `valor`, `data`) VALUES
+(1, 'Papel Sulfite', 350, '2024-04-12'),
+(2, '10 Grampeadores', 75, '2024-04-13'),
+(3, '30 canetas bic azuis', 26, '2024-04-13'),
+(4, 'Manutenção impressora HP 3660', 90, '2024-04-13');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fornecedor`
+--
+
+CREATE TABLE `fornecedor` (
+  `id` int(11) NOT NULL,
+  `razao_social` varchar(256) NOT NULL,
+  `cnpj` varchar(256) NOT NULL,
+  `telefone` varchar(11) NOT NULL,
+  `email` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `fornecedor`
+--
+
+INSERT INTO `fornecedor` (`id`, `razao_social`, `cnpj`, `telefone`, `email`) VALUES
+(2, 'Empresa 1', '11111111111111', '11111111111', 'empresa1@email.com'),
+(3, 'Empresa 2', '2222222222', '222222222', 'empresa2@email.com'),
+(4, 'Empresa 3', '333333333333', '333333333', 'empresa3@email.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(256) NOT NULL,
+  `valor` decimal(10,0) NOT NULL,
+  `categoria` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `valor`, `categoria`) VALUES
+(1, 'Teclado', 250, 'Eletrônicos'),
+(2, 'Mouse', 194, 'Eletrônicos'),
+(3, 'HUB USB', 60, 'Eletrônicos'),
+(4, 'Mousepad', 113, 'Eletrônicos');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `despesa`
+--
+ALTER TABLE `despesa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `despesa`
+--
+ALTER TABLE `despesa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
